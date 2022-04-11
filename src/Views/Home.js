ed941c9e8b78c74c3180b2fc654a07f81880cda1
m-ghiringhelli <mghiring@gmail.com>
2022-04-11 12:56:14 -0700

    testing function to map through string

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -2,5 +2,38 @@ import React from 'react';
 import * as Tone from 'tone';
 
 export default function Home() {
-  return <div>Home</div>;
+  const synth = new Tone.Synth().toDestination();
+
+  const noteData = {
+    A: 'A4',
+    S: 'B4',
+    D: 'C4',
+  };
+
+  const testString = 'abc';
+
+  const turnCharToNote = (char) => {
+    const testChar = char.toUpperCase();
+    console.log(noteData.testChar);
+  };
+
+  const playString = (str) => {
+    // console.log(str, 'in playstring');
+    str.map((char) => {
+      turnCharToNote(char);
+      // const note = turnCharToNote(char);
+      // playNote(note);
+    });
+  };
+
+  playString(testString.split(''));
+
+  const playNote = () => {
+    synth.triggerAttackRelease('C4', '8n');
+  };
+  return <div>
+    <textarea onChange={playNote}>
+    
+    </textarea>
+  </div>;
 }
