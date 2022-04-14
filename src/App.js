1ea350200a1ff64932850ae7f9e1003ee653b2ee
Alex Orlet <AlexOrlet89@gmail.com>
2022-04-13 17:33:54 -0700

    about us section info added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from './Views/Home';
 import Profile from './Views/Profile';
 import Edit from './Views/Edit';
 import backgroundImage from './Images/background.png';
+import AboutUs from './Views/AboutUs';
 
 function App() {
   const { currentUser } = useUserContext();
@@ -26,6 +27,9 @@ function App() {
       <BrowserRouter>
         <Header />
         <Switch>
+          <Route exact path="/aboutus">
+            <AboutUs />
+          </Route>
           <Route path="/auth">{!currentUser ? <Auth /> : <Redirect exact to="/" />}</Route>
           <Route exact path="/">
             <Home />
